9392ae59202976582dc70693809d3dd96c7e2b0d
Gopi <gopikrishna.gummadidala@gmail.com>
2021-05-02 14:04:14 +0200

    latest code changes

--- WebSocketsDemoApp/WebSocketsDemo.Web/client-app/src/app/app.component.ts
+++ WebSocketsDemoApp/WebSocketsDemo.Web/client-app/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Square } from "src/models/square";
+import { SquareChangeRequest } from "src/models/square-change-request";
+import { WebSocketService } from "src/web-socket.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'client-app';
+  announcementSub;
+  messages: string[] = [];
+  squares: Square[] = [];
+  colors: string[] = ["red", "green", "blue"];
+  currentColor: string = "red";
+  name: string = "";
+  constructor(private socketService: WebSocketService) {
+    this.socketService.announcement$.subscribe((announcement) => {
+      if (announcement) {
+        this.messages.unshift(announcement);
+      }
+    });
+    this.socketService.squares$.subscribe((sq) => {
+      this.squares = sq;
+    });
+    this.socketService.name$.subscribe((n) => {
+      this.name = n;
+    });
+  }
+
+  ngOnInit() {
+    this.socketService.startSocket();
+  }
+
+  squareClick(event, square: Square) {
+    if (square.Color === this.currentColor) return;
+    var req = new SquareChangeRequest();
+    req.Id = square.Id;
+    req.Color = this.currentColor;
+    this.socketService.sendSquareChangeRequest(req);
+  }
 }
